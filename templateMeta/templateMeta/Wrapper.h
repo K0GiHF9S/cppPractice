9588835509677753329ef4b5c6b6a12615e9b3f9
H/S <kidoairakusyunkasyuutou@gmail.com>
2018-11-20 19:31:18 +0900

    IsTemplateOf

--- templateMeta/templateMeta/Wrapper.h
+++ templateMeta/templateMeta/Wrapper.h
@@ -1,2 +1,14 @@
 #pragma once
+#include <type_traits>
 
+template<class, template<class...> class>
+struct IsTemplateOf : std::false_type {};
+
+template<class... T, template<class...> class TTemplate>
+struct IsTemplateOf<TTemplate<T...>, TTemplate> : std::true_type {};
+
+template <class T>
+constexpr bool IsMapOf(void)
+{
+	return (IsTemplateOf<T, std::map>::value);
+}
